e365f9e391527e7712272521800ac947a52d9195
Shumail Abdulrehman <47891@students.riphah.edu.pk>
2026-03-22 16:26:55 +0500

    added routes for all contorllers

--- backend/src/routes/location.route.ts
+++ backend/src/routes/location.route.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { createLocation, editLocation, getLocations, softDeleteLocation } from "../controllers/location.controller.js";
+import { createLocation, editLocation, getLocations, softDeleteLocation, getInactiveLocations,getLocationById, getLocationStatsById, restoreLocation} from "../controllers/location.controller.js";
 import { verifyJwt } from "../middlewares/auth.middleware.js";
 import authorize from "../middlewares/authorize.middleware.js";
 
@@ -9,5 +9,10 @@ router.post("/", verifyJwt, authorize, createLocation);
 router.get("/", verifyJwt, authorize, getLocations);
 router.patch("/:id", verifyJwt, authorize, editLocation);
 router.patch("/:id/deactivate", verifyJwt, authorize, softDeleteLocation);
+router.get("/inactive", verifyJwt, authorize, getInactiveLocations);
+router.get("/:id", verifyJwt, authorize, getLocationById);
+router.get("/:id/stats", verifyJwt, authorize, getLocationStatsById);
+router.patch("/:id/restore", verifyJwt, authorize, restoreLocation);
 
+    
 export default router;
--- backend/src/routes/staff.route.ts
+++ backend/src/routes/staff.route.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { loginStaff, logoutStaff, createStaff, getStaff, softDeleteStaff } from "../controllers/staff.controller.js";
+import { loginStaff, logoutStaff, createStaff, getStaff, softDeleteStaff , getInactiveStaff, getStaffById, getStaffByLocation, getProfile, refreshStaffToken   } from "../controllers/staff.controller.js";
 import { verifyJwt } from "../middlewares/auth.middleware.js";
 import authorize from "../middlewares/authorize.middleware.js";
 
@@ -11,6 +11,12 @@ router.post("/staff-logout", verifyJwt, logoutStaff);
 router.post("/create-staff", verifyJwt, authorize, createStaff);
 router.get("/", verifyJwt, authorize, getStaff);
 router.patch("/:id/deactivate", verifyJwt, authorize, softDeleteStaff);
-// router.patch("/:id/active",verifyJwt,authorize)
+router.get("/inactive", verifyJwt, authorize, getInactiveStaff);
+router.get("/:id", verifyJwt, authorize, getStaffById);
+router.get("/location/:locationId", verifyJwt, authorize, getStaffByLocation);
+router.get("/profile/me", verifyJwt, getProfile);
+router.post("/refresh-token", refreshStaffToken);
+
+
 
 export default router;
