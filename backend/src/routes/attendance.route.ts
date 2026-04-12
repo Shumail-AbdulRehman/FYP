a340a4a10f88df22fdaac1e12d5eb3e46981ec7c
Shumail Abdulrehman <47891@students.riphah.edu.pk>
2026-04-12 19:50:21 +0500

    added multer middleware to parse images

--- backend/src/routes/attendance.route.ts
+++ backend/src/routes/attendance.route.ts
@@ -2,12 +2,14 @@ import { Router } from "express";
 import { checkIn, checkOut, getMyAttendance, getStaffAttendance, assignShiftToStaff } from "../controllers/attendance.controller.js";
 import { verifyJwt } from "../middlewares/auth.middleware.js";
 import authorize from "../middlewares/authorize.middleware.js";
+import upload from "../middlewares/upload.middleware.js";
+
 
 const router = Router();
 
 router.patch("/staff/:id/shift", verifyJwt, authorize, assignShiftToStaff);
-router.post("/check-in", verifyJwt, checkIn);
-router.post("/check-out", verifyJwt, checkOut);
+router.post("/check-in", verifyJwt,upload.single("image"), checkIn);
+router.post("/check-out",upload.single("image") ,verifyJwt, checkOut);
 router.get("/my", verifyJwt, getMyAttendance);
 router.get("/", verifyJwt, authorize, getStaffAttendance);
 
