02aef4a5645d284a0b8abe89735a2818312d4325
Shumail Abdulrehman <47891@students.riphah.edu.pk>
2026-04-12 23:55:56 +0500

    updated the route for photo verification for task completion

--- backend/src/routes/taskInstance.route.ts
+++ backend/src/routes/taskInstance.route.ts
@@ -2,13 +2,14 @@ import {getTodaysTasksForStaff, startTask, completeTask, getTaskInstanceById, ge
 import { Router } from "express";
 import { verifyJwt } from "../middlewares/auth.middleware.js";
 import authorize from "../middlewares/authorize.middleware.js";
+import upload from "../middlewares/upload.middleware.js";
 
 const router = Router();
 
-router.get("/staff/:staffId/today", verifyJwt, authorize, getTodaysTasksForStaff);
-router.post("/:taskId/start", verifyJwt, authorize, startTask);
-router.post("/:taskId/complete", verifyJwt, authorize, completeTask);
-router.get("/:taskId", verifyJwt, authorize, getTaskInstanceById);
+router.get("/staff/:staffId/today", verifyJwt, getTodaysTasksForStaff);
+router.post("/:taskId/start", verifyJwt, startTask);
+router.post("/:taskId/complete", verifyJwt,upload.array("images", 5), completeTask);
+router.get("/:taskId", verifyJwt, getTaskInstanceById);
 router.get("/location/:locationId", verifyJwt, authorize, getTasknstancesOfLocation);
 
 
